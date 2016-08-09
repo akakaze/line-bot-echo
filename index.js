220580f4e78bb011475ba3ea78a867d546236bce
akakaze <m2251000@gmail.com>
2016-08-10 06:04:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false, limit: 2 * 1024 * 1024 }));
 app.use(bodyParser.json({ limit: 2 * 1024 * 1024 }));
 
 app.post('/', function (req, res) {
-  console.log(req.body.result);
+  //console.log(req.body.result);
 
   var receives = client.createReceivesFromJSON(req.body);
   _.each(receives, function(receive){
