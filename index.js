0a2094dd92b3fae1da57525aa732d16ca21fc4d1
akakaze <m2251000@gmail.com>
2016-08-14 15:48:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/proxy', proxy(process.env.FIXIE_URL));
+app.use('/', proxy(process.env.FIXIE_URL));
 app.use(bodyParser.urlencoded({ extended: false, limit: 2 * 1024 * 1024 }));
 app.use(bodyParser.json({ limit: 2 * 1024 * 1024 }));
 
