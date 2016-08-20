367680a6f8fc5ca61c374c783942a545ce8afab4
akakaze <m2251000@gmail.com>
2016-08-20 16:08:53 +0800

    test no sdk

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.set("port", (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.post("/", function(req, res){
+app.post("/callback", function(req, res){
 	async.waterfall([function(callback) {
 		var json = req.body;
 		var from = json["result"][0]["content"]["from"];
